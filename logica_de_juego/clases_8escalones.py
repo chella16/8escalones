aa112c5d7e5881d6ae3706e556f618d23e809340
cavi1 <52816119+cavi1@users.noreply.github.com>
2024-11-09 03:14:34 -0300

    Update clases_8escalones.py

--- logica_de_juego/clases_8escalones.py
+++ logica_de_juego/clases_8escalones.py
@@ -4,7 +4,7 @@ class Jugador:
     
     def __init__(self, nombre): #¿Qué sabe hacer un jugador? Para mi, un jugador sabe decir su nombre, sabe responder preguntas y decir si está eliminado o no
         self.__nombre=nombre #Pero no es capaz de eliminarse, el solo puede decirte si está eliminado o no
-        self.eliminado=False
+        self.__eliminado=False
         
     def responder_pregunta(self, pregunta) -> str:
         respuesta=pregunta.verificar_respuesta(pregunta.get_consigna())
@@ -16,8 +16,9 @@ class Jugador:
     def get_estado(self) -> str:
         return self.__eliminado
     
-    #Opcion 1: hago el estado del jugador público (no respeto hacer atributos privados) 
-    #Opción 2: creo el set eliminado en jugador pero sería raro porque un jugador no se sabe eliminar #¿Cual elijo? (por ahora lo dejo publico y lo elimino en el juego)
+    def marcar_eliminado(self):
+        self.__eliminado=True
+    
     
 class Pregunta:
     def __init__(self, tematica): #Pregunta solo tiene la capacidad de verificar si una palabra coincide con su respuesta almacenada
