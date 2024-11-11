b2fa7961536107b3b991ebb7a5485887834b19a9
cavi1 <52816119+cavi1@users.noreply.github.com>
2024-11-11 03:14:45 -0300

    Update pregunta_comun.py

--- modelo_8escalones/clases/pregunta/pregunta_comun.py
+++ modelo_8escalones/clases/pregunta/pregunta_comun.py
@@ -4,7 +4,7 @@ class Pregunta_comun(Pregunta):#Una pregunta común tiene: opciones, y el métod
         super().__init__()
         self.__opciones = []
     
-    def obtener_pregunta_bd(self,tema,id): #en el caso de la pregunta común lo pensé como para que te lleves todas las preguntas de una temática de forma seccuencial, ya que si le pasara un id random no tengo forma de asegurarme que no se repita
+    def obtener_pregunta_bd(self,tema,id): #le cae un id random que nunca va a estar repetido por instancia de escalon
         pass #consulta para obtener la info sobre la pregunta en este caso es la pregunta común
     
     def get_opciones(self):
