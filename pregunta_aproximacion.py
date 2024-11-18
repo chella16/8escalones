03bb44140e75053d96d99c6ce7d049078098afb0
chella16 <166649236+chella16@users.noreply.github.com>
2024-11-17 23:07:39 -0300

    preguntas

--- pregunta_aproximacion.py
+++
@@ -1,8 +0,0 @@
-from pregunta import Pregunta
-
-class Pregunta_aproximacion(Pregunta):
-    def __init__(self, tema, consigna, rta, dificultad):
-        super().__init__(tema, consigna, rta, dificultad)
-        
-    #def responder_pregunta_aproximacion(self):
-        #pass  #es un input por parte del jugador ?¿
